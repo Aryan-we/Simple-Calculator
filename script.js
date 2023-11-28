d1ddb4ba45a6ed1e0f0d9eb08ec4f32f1eb2fc7e
Aryan-we <96795631+Aryan-we@users.noreply.github.com>
2023-11-28 14:50:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+function offf(){
+    document.getElementById('1').disabled=true;
+    document.getElementById('2').disabled=true;
+    document.getElementById('3').disabled=true;
+    document.getElementById('4').disabled=true;
+    document.getElementById('5').disabled=true;
+    document.getElementById('6').disabled=true;
+    document.getElementById('7').disabled=true;
+    document.getElementById('8').disabled=true;
+    document.getElementById('9').disabled=true;
+    document.getElementById('0').disabled=true;
+    document.getElementById('result').disabled=true;
+   
+    document.getElementById('clear').disabled=true;
+    document.getElementById('=').disabled=true; 
+      document.getElementById('+').disabled=true;
+    document.getElementById('-').disabled=true;
+    document.getElementById('*').disabled=true;
+    document.getElementById('÷').disabled=true;
+    document.getElementById('%').disabled=true;
+}     
+function onn(){
+    document.getElementById('1').disabled=false;
+    document.getElementById('2').disabled=false;
+    document.getElementById('3').disabled=false;
+    document.getElementById('4').disabled=false;
+    document.getElementById('5').disabled=false;
+    document.getElementById('6').disabled=false;
+    document.getElementById('7').disabled=false;
+    document.getElementById('8').disabled=false;
+    document.getElementById('9').disabled=false;
+    document.getElementById('0').disabled=false;
+    document.getElementById('result').disabled=false;
+   
+    document.getElementById('clear').disabled=false;
+    document.getElementById('=').disabled=false; 
+    document.getElementById('+').disabled=false;
+    document.getElementById('-').disabled=false;
+    document.getElementById('*').disabled=false;
+    document.getElementById('÷').disabled=false;
+    document.getElementById('%').disabled=false;
+}     
+function clearr(){
+    document.getElementById('result').value="";    
+} 
+
